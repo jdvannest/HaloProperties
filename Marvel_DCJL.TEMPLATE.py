0ae31a989ca8ab8afa8a1f47bf640eea25dbd243
jdvannest <jordanvannest@gmail.com>
2021-07-23 17:43:58 -0500

    Added comments to TEMPLATE file

--- Marvel_DCJL.TEMPLATE.py
+++ Marvel_DCJL.TEMPLATE.py
@@ -14,12 +14,16 @@ parser.add_argument("-p","--path",required=True)
 parser.add_argument("-n","--numproc",required=True,type=int)
 args = parser.parse_args()
 
-Sims = pickle.load(open('SimulationInfo.pickle','rb'))
 
+#########################################################################
 #Change the name of the property you're collecting (used for file naming)
 property = 'TEST'
+#########################################################################
 
 
+#Load in Simulation Info (paths and halo numbers)
+Sims = pickle.load(open('SimulationInfo.pickle','rb'))
+#Load in the existing data file, or create new/overwrite if necessary
 file_name = args.path+f'Marvel_DCJL.{property}.pickle'
 if args.overwrite:
     os.system('rm '+file_name)
@@ -39,38 +43,57 @@ else:
 
 
 sim  = args.simulation
+#Skip if this simulation if it has already been done (unless overwriting)
 if len(Data[sim])>0:
     print(f'{sim} already completed')
 else:
     print(f'Loading {sim}...')
+    #Create shared dictionary for multiprocessing data and shared 1-element
+    # array to act as scalar for traking progress
     SimData = pymp.shared.dict()
+    prog=pymp.shared.array((1,),dtype=int)
+    #Load in simulation
     s = pynbody.load(Sims[sim]['path'])
     s.physical_units()
     h = s.halos()
     myprint(f'{sim} Loaded.',clear=True)
     print('\tWriting: 0%')
-    prog=pymp.shared.array((1,),dtype=int)
+    #Begin parallelized analysis
     with pymp.Parallel(args.numproc) as pl:
         for i in pl.xrange(len(Sims[sim]['halos'])):
+            #Load in specific halo
             halonum = Sims[sim]['halos'][i]
-            current={}
             halo = h[halonum]
 
-            #Replace this block with the desired property to collect
-            print(f'{sim} : {halonum}')
-            current['test'] = 1
+            #The shared dictionary object does not allow writing to sub-dictionaries
+            #So use 'current' as a 'middle-man' step that will be inserted as a whole
+            #into the shared dictionary at the end
+            current={}
 
+            ########################################################
+            #Replace this block with the desired property to collect
+            current['test_property_1'] = 1
+            current['test_property_2'] = [3,6,9]
+            current['test_property_3'] = {'a':1,'b':[1,2,3]}
+            ########################################################
             
+
+            #Print progress to terminal
             myprint(f'\tWriting: {round(float(prog[0]+1)/float(len(Sims[sim]["halos"]))*100,2)}%',clear=True)
-            #with pl.lock:
+            
+            #with pl.lock:  <----- the 'lock' block probably isn't necessary since the only thing that might
+            #                    encounter a race condition is the progress counter, which is only for the UI
+            #Insert the current halo's data into the shared dictionary
             SimData[str(halonum)] = current
             prog[0]+=1
             del current
 
-
+    #Transfer the data from the shared dictionary to the main Data File (saving the shared
+    #dictionary just results in garbage data for some reason)
     for halo in Sims[sim]['halos']:
         Data[sim][str(halo)] = SimData[str(halo)]
 
+    #Update the Data File
     out = open(file_name,'wb')
     pickle.dump(Data,out)
     out.close()
