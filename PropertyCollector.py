862fa6a36e6ac1a9c1c3ec45b8bdce2bc8aff65e
jdvannest <jordanvannest@gmail.com>
2021-07-23 17:20:19 -0500

    Added simulation info to separate file for easy macro editing. Adding addition args to main executable.

--- PropertyCollector.py
+++ PropertyCollector.py
@@ -1,12 +1,23 @@
 import os,argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-p","--property",required=True)
-parser.add_argument("-o","--overwrite",action='store_true')
+parser = argparse.ArgumentParser(description='This script iterates through'+
+            'the Marvel+DCJL resolved dwarf halos and collects the desired'+
+            'property. The data is written out as a dictionary in a .pickle'+
+            'file, and is read as DataFile[simulation][halo][property]')
+parser.add_argument("-p","--property",required=True,
+                    help='the desired property to be collected')
+parser.add_argument("-o","--overwrite",action='store_true',
+                    help='Add flag to overwrite existing datafile')
+parser.add_argument("-n","--numproc",type=int,default=1,
+                    help='The number of processes to use (default=1)')
 args = parser.parse_args()
 
+#Path to directory where datafiles are written
+output_path = '/myhome2/users/vannest/Data/'
+#Path to prefered python executable
+python_path = '/myhome2/users/vannest/anaconda3/bin/python'
+
 for sim in ['cptmarvel','elektra','storm','rogue','h148','h229','h242','h329']:
-    if sim=='cptmarvel' and args.overwrite:
-        os.system(f'/myhome2/users/vannest/anaconda3/bin/python /home/vannest/Code/Marvel_DCJL.{args.property}.py -s {sim} -o')
-    else:
-        os.system(f'/myhome2/users/vannest/anaconda3/bin/python /home/vannest/Code/Marvel_DCJL.{args.property}.py -s {sim}')
+    o_flag = '-o' if sim=='cptmarvel' and args.overwrite else ''
+    os.system(f'{python_path} Marvel_DCJL.{args.property}.py -s {sim} '+
+              f'-p {output_path} -n {args.numproc} {o_flag}')
